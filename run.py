7f989ba63401085b49daaa8b28eb95bd9a51f28e
FakkuGZ <fakku5@live.com.ar>
2024-06-30 22:25:36 -0300

    proyecto completo

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app.database import init_app
+from app.views import *
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+init_app(app)
+CORS(app, resources={r"/*": {"origins": "*"}}) 
+
+app.route("/gestion/", methods=["GET"])(get_all_products)
+app.route("/gestion/<int:product_id>", methods=["GET"])(get_product)
+app.route("/gestion/", methods=["POST"])(create_product)
+app.route("/gestion/<int:product_id>", methods=["PUT"])(update_product)
+app.route('/gestion/<int:product_id>', methods=['DELETE'])(delete_product)
+
+if __name__ == "__main__":
+    app.run(debug=True)
